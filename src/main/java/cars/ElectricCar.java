7c628aebeaac85ee16f3a7f99463cf76923cd2bd
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-31 14:15:47 +0100

    ElectricCar class created and tested.

--- src/main/java/cars/ElectricCar.java
+++ src/main/java/cars/ElectricCar.java
@@ -1,4 +1,10 @@
 package cars;
 
-public class ElectricCar {
+public class ElectricCar extends Car{
+
+    public ElectricCar(int price, String make, String model, String colour, Gearbox gearbox, Tyre tyre, Engine engine) {
+        super(price, make, model, colour, gearbox, tyre, engine);
+
+        this.type = "electric";
+    }
 }
--- src/test/java/ElectricCarTest.java
+++ src/test/java/ElectricCarTest.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import cars.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class ElectricCarTest {
+    ElectricCar electricCar;
+    Engine engine;
+    Gearbox gearbox;
+    Tyre tyre;
+
+
+    @Before
+    public void setUp() {
+        engine = new Engine(100,"electric");
+        gearbox = new Gearbox("Automatic",6);
+        tyre = new Tyre("Michelin", "E15", "Sport");
+        electricCar = new ElectricCar(5000, "Toyota", "Zippo", "Blue", gearbox, tyre, engine);
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(5000, electricCar.getPrice());
+    }
+
+    @Test
+    public void hasMake(){
+        assertEquals("Toyota", electricCar.getMake());
+    }
+
+    @Test
+    public void hasModel(){
+        assertEquals("Zippo", electricCar.getModel());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("Blue", electricCar.getColour());
+    }
+
+    @Test
+    public void hasTyre(){
+        assertEquals(tyre, electricCar.getTyre());
+    }
+
+    @Test
+    public void hasEngine() {
+        assertEquals(engine, electricCar.getEngine());
+    }
+
+    @Test
+    public void hasGearBox(){
+        assertEquals(gearbox, electricCar.getGearbox());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals("electric", electricCar.getType());
+    }
+
+
 }
