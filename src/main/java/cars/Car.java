c3a93b5eb8f8ab0523914dbc80ab07afbe59c2e7
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-31 13:33:39 +0100

    added car class containing all components and additional variables

--- src/main/java/cars/Car.java
+++ src/main/java/cars/Car.java
@@ -1,4 +1,62 @@
 package cars;
 
-public class Car {
+public abstract class Car {
+    private int price;
+    private String make;
+    private String model;
+    private String engineType;
+    private String colour;
+    private Gearbox gearbox;
+    private Tyre tyre;
+    private Engine engine;
+
+    public Car(int price,
+               String make,
+               String model,
+               String engineType,
+               String colour,
+               Gearbox gearbox,
+               Tyre tyre,
+               Engine engine) {
+        this.price = price;
+        this.make = make;
+        this.model = model;
+        this.engineType = engineType;
+        this.colour = colour;
+        this.gearbox = gearbox;
+        this.tyre = tyre;
+        this.engine = engine;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public Gearbox getGearbox() {
+        return gearbox;
+    }
+
+    public Tyre getTyre() {
+        return tyre;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
 }
