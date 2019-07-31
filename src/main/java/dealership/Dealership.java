d89a700e534c5f8fb53e53a63e2933d8310386cc
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-31 15:03:40 +0100

    Status at end of lab. Working on Dealership class.

--- src/main/java/dealership/Dealership.java
+++ src/main/java/dealership/Dealership.java
@@ -1,4 +1,29 @@
 package dealership;
 
+import cars.Car;
+
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private ArrayList<Car> carStock;
+    private Till till;
+    private Dealer dealer;
+    private Customer customer;
+
+    public Dealership(String name, ArrayList<Car> carStock, Till till, Dealer dealer, Customer customer) {
+        this.name = name;
+        this.carStock = carStock;
+        this.till = till;
+        this.dealer = dealer;
+        this.customer = customer;
+    }
+
+    public void removeCar(Car car){
+        carStock.remove(car);
+    }
+
+    public void addToTill(int cash){
+        this.till.addMoney(cash);
+    }
 }
