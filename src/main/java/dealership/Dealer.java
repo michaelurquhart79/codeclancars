eb3e21802692b2f987e6bdc2807b908589c73d19
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-31 15:03:06 +0100

    added untested Dealer class.

---
+++ src/main/java/dealership/Dealer.java
@@ -0,0 +1,21 @@
+package dealership;
+
+import cars.Car;
+
+public class Dealer {
+    private String name;
+
+    public Dealer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public sellCar(Customer customer, Car car){
+//        //remove car from car array
+//        //get funds from customer
+//        //add funds to till
+//    }
+}
